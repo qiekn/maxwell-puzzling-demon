136d6913f6ae52f03a3e496e312ba90b24a2dd66
qiekn <qiekn@kimidayo.cn>
2025-04-06 00:08:43 +0800

    fix: merge system

--- Assets/_Script/Core/Systems/MergeSystem.cs
+++ Assets/_Script/Core/Systems/MergeSystem.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 namespace qiekn.core {
     public class MergeSystem {
         private static MergeSystem instance;
-        readonly Queue<Crate> queue = new();
+        private readonly Queue<Crate> queue = new();
 
-        public static MergeSystem Instance {
-            get {
-                instance ??= new MergeSystem();
-                return instance;
-            }
-        }
+        public static MergeSystem Instance => instance ??= new MergeSystem();
 
         public void Register(Crate crate) {
             queue.Enqueue(crate);
@@ -22,38 +17,37 @@ namespace qiekn.core {
                 var crate = queue.Dequeue();
                 KissKiss(crate, crate.GM.GetAdjacent<Crate>(crate, AdjacentFilter.Merge));
             }
-        }
 
-        // merge function
-        void KissKiss(Crate me, HashSet<Crate> set) {
-            foreach (var x in set) {
-                Debug.Log("MergeSystem: " + me.position + " merge with " + x.position);
-            }
-            foreach (var lover in set) {
-                // private dating
-                lover.UnRegister();
-                me.UnRegister();
-                // merge shape & units
-                foreach (var offset in lover.offsets) {
-                    var me_offset = lover.position + offset - me.position;
-                    me.offsets.Add(me_offset);
-
-                    var unit = lover.units[offset];
-                    unit.position = me_offset;
-                    foreach (var pair in unit.borders) {
-                        pair.Value.pos = me_offset;
+            // merge function
+            void KissKiss(Crate me, HashSet<Crate> set) {
+                foreach (var x in set) {
+                    Debug.Log("MergeSystem: " + me.position + " merge with " + x.position);
+                }
+                foreach (var lover in set) {
+                    // private dating
+                    lover.UnRegister();
+                    me.UnRegister();
+                    // merge offset & units
+                    foreach (var old_offset in lover.offsets) {
+                        var new_offset = lover.position + old_offset - me.position;
+                        me.offsets.Add(new_offset);
+                        var unit = lover.units[old_offset];
+                        unit.position = new_offset;
+                        foreach (var pair in unit.borders) {
+                            pair.Value.pos = new_offset;
+                        }
+                        me.units.Add(new_offset, unit);
                     }
-                    me.units.Add(me_offset, unit);
+                    // merge the list of borders' reference
+                    foreach (var border in lover.borders) {
+                        me.borders.Add(border);
+                    }
+                    // submit
+                    lover.Destory();
+                    me.Register();
+                    me.DisableInnerPairedStickyBorders();
+                    me.UpdateSprites();
                 }
-                // merge borders
-                foreach (var border in lover.borders) {
-                    me.borders.Add(border);
-                }
-                // submit
-                lover.Destory();
-                me.Register();
-                me.DisableInnerPairedStickyBorders();
-                me.UpdateSprites();
             }
         }
     }
